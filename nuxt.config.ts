03cff7a5ba02f8b3bb5c9a1e28afc77b3f2da2e9
David Straub <scinvention@gmail.com>
2022-11-26 23:11:59 -0500

    chore(nuxt.config): set up for reverse proxy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  vite: {
+    server: {
+      strictPort: true,
+      // Reverse Proxy
+      // https://github.com/nuxt/framework/issues/1021#issuecomment-947123482
+      hmr: {
+        protocol: 'wss',
+        clientPort: 443,
+        path: 'hmr/',
+      }
+    }
+  }
 })
