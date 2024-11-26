48963f77341990e93d9da5a2a17aa2402836372a
David Straub <scinvention@gmail.com>
2024-11-26 17:41:35 -0500

    feat: service worker caching

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,11 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'mask-icon', href: '/mask-icon.svg', color: '#000000' },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+        { rel: 'preconnect', crossorigin: 'anonymous', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', crossorigin: 'anonymous', href: 'https://fonts.gstatic.com' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400&display=swap', crossorigin: 'anonymous' },
       ],
       htmlAttrs: {
         lang: 'en',
@@ -32,9 +37,9 @@ export default defineNuxtConfig({
   modules: ['@vite-pwa/nuxt'],
 
   pwa: {
-    // devOptions: {
-    //   enabled: true
-    // },
+    devOptions: {
+      enabled: process.env.NODE_ENV === 'development',
+    },
     includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
     manifest: {
       name: 'Wuxia Wednesday',
@@ -55,6 +60,78 @@ export default defineNuxtConfig({
           type: 'image/png'
         }
       ]
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'google-fonts-cache',
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        },
+        {
+          urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'gstatic-fonts-cache',
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+          }
+        },
+        {
+          urlPattern: /^https:\/\/api\.themoviedb\.org\/\d+\/search\/.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'tmdb-search-cache',
+            expiration: {
+              maxAgeSeconds: 60 * 60 * 1 // <== 1 hour
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+          }
+        },
+        {
+          urlPattern: /^https:\/\/api\.themoviedb\.org\/\d+\/movie\/popular.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'tmdb-popular-cache',
+            expiration: {
+              maxAgeSeconds: 60 * 60 * 24 * 1 // <== 1 day
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+          }
+        },
+        {
+          urlPattern: /^https:\/\/api\.themoviedb\.org\/\d+\/(?:movie|person)\/(?!.*popular).*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'tmdb-api-cache',
+            expiration: {
+              maxAgeSeconds: 60 * 60 * 24 * 30 // <== 30 days
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+          }
+        },
+      ],
     },
   },
 
